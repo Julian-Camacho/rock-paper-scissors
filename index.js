93e051f79a2ce26676113740015a3dcd67867f1a
Julian Camacho <julian.francama@gmail.com>
2023-04-24 19:19:17 -0300

    Agregué función de juego

--- index.js
+++ index.js
@@ -3,7 +3,36 @@ console.log("hello, world!")
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    console.log("Compuuuuter");
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
 }
 
-getComputerChoice();
+function checkWin(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "tie";
+    } else if(
+        (playerSelection == "rock" && computerSelection == "scissors") || 
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "player";
+    } else {
+        return "computer";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    const winner = checkWin(playerSelection, computerSelection);
+    if(winner == "tie"){
+        return "It's a Tie!";
+    } else if(winner == "player"){
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
